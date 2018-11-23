7ccf05eb7a329fd265d789e3beff2d05d1c07028
Arutosio <Arutosio@outlook.com>
2018-11-23 13:21:43 +0100

    Update
    Nuovi funzioni per implementare successivamente nuove funzionalità

--- js/tercon.js
+++ js/tercon.js
@@ -4,28 +4,42 @@
 var screen = "";
 //Event Keydown
 document.addEventListener("keydown", keyDown);
+
 function keyDown(keyP) {
     if (document.getElementById("cmdLine").value && 13 === (keyP.which || keyP.keyCode))
-        executeCMD();
+        startToDo();
 }
-function executeCMD() {
-    var cmd = document.getElementById("cmdLine");
+function startToDo() {
+    var cmd = document.getElementById("cmdLine").value;
+    document.getElementById("cmdLine").value = "";
     innerLine(cmd);
-    cmd.value = "";
 }
 function innerLine(cmd) {
-    var line = checkLine(cmd.value);
-    line = checkLine(line)
-    line = line +'<br/>';
-    screen = screen + line;
+    var line = checkLine(cmd);
+    executeCMD(line);
+    screen = screen + line +'<br/>';
     document.getElementById("screen").innerHTML = screen;
 }
 function checkLine(line) {
     line = " " + line;
-    line.replace(' sudo ','<strong style="color:red"> sudo </strong>');
-    line.replace(' sudo ','<strong style=\'color:red\'> sudo </strong>');
-    line.replace(' sudo ',"<strong style='color:red'> sudo </strong>");
-
+    //problem: non vengono aggiunti gli elelemnti <> sulla stringa
+    line.replace(' info','<strong style="color:red"> info </strong>');
+    line.replace(' help','<strong style="color:red"> help </strong>');
+    line.replace(' cal ','<strong style="color:red"> cal </strong>');
     return line;
+}
+function executeCMD(cmdLine) {
+    var cmdSplit = makeArray(cmdLine)
+    if(cmdLine.indexOf(' info ') !== -1)
+        alert('there is! info!!!');
+        if(cmdLine.indexOf('> help') !== -1)
+        alert('there is! help!!!');
+        if(cmdLine.indexOf('> cal') !== -1)
+        alert('there is! cal!!!');
+}
+function makeArray(cmdLine) {
+    cmdLine = cmdLine.split(' '); // split string on comma space
+    console.log(cmdLine); // [ "apple", "orange", "pear", "banana", "raspberry", "peach" ]
+    return cmdLine
 }
 
